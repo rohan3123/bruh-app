fba37f59b8c8eeaab596b4acf9d238a531e98048
Rohan <rohangazi30@gmail.com>
2023-08-31 21:15:41 +0100

    created bruh mp3 and added code to onclick

--- bruh/app/src/main/java/rohan/app/bruh/MainActivity.java
+++ bruh/app/src/main/java/rohan/app/bruh/MainActivity.java
@@ -1,14 +1,43 @@
 package rohan.app.bruh;
 
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private Button playButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the MediaPlayer
+        mediaPlayer = MediaPlayer.create(this, R.raw.bruh);
+
+        // Get reference to the button from the layout
+        playButton = findViewById(R.id.button2);
+
+        // Set click listener for the button
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!mediaPlayer.isPlaying()) {
+                    mediaPlayer.start(); // Start playing the sound
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mediaPlayer != null) {
+            mediaPlayer.release(); // Release the MediaPlayer resources when the activity is destroyed
+        }
     }
 }
